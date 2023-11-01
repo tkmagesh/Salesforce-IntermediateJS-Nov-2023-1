5653fb85366914ab3e5d4b7582eedd2cd47c9ef6
Magesh Kuppan <tkmagesh77@gmail.com>
2023-11-02 04:39:18 +0530

    spinner (closure) - problem statement

---
+++ 04-closure/spinner.js
@@ -0,0 +1,33 @@
+/* 
+    Create an object and assign to a variable 'spinner'
+    
+    The object must have the following methods:
+    - up()
+    - down()
+    
+    Method Behavior:
+    spinner.up() //=> 1
+    spinner.up() //=> 2
+    spinner.up() //=> 3
+    spinner.up() //=> 4
+
+    spinner.down() //=> 3
+    spinner.down() //=> 2
+    spinner.down() //=> 1
+    spinner.down() //=> 0
+    spinner.down() //=> -1
+
+    IMPORTANT:
+        The outcome of the up() & down() methods SHOULD NOT be able to be influenced from outside
+
+        The following should NOT be possible
+        window.count = 1000
+        spinner.up() //=> 1001
+
+        OR
+
+        spinner.count = 1000
+        spinner.up() //=> 1001
+*/
+
+var spinner = /* ...... */
