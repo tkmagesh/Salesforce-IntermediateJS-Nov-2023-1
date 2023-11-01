53424ae84422ab64ddf8ef5adad518a510c7582b
Magesh Kuppan <tkmagesh77@gmail.com>
2023-11-02 01:05:50 +0530

    language - exercise - setup

---
+++ 03-language-exercise/calculator.test.js
@@ -0,0 +1,76 @@
+function sum(x,y){
+    return x + y
+}
+
+test('sum(10,20) => 30', function(){
+    //arrange
+    var x = 10,
+        y = 20,
+        expectedResult = 30
+
+    //act
+    var actualResult = sum(x,y);
+
+    //assert
+    expect(actualResult).toBe(expectedResult)
+})
+
+test('sum(10, "20") => 30', function(){
+    var expectedResult = 30
+    var actualResult = sum(10, "20")
+    expect(actualResult).toBe(expectedResult)
+})
+
+test('sum(10, "abc") => 10', function(){
+    var expectedResult = 10
+    var actualResult = sum(10, "abc")
+    expect(actualResult).toBe(expectedResult)
+})
+
+test('sum(10,20,30,40,50) => 150', function(){
+    var expectedResult = 150
+    var actualResult = sum(10, 20, 30, 40, 50)
+    expect(actualResult).toBe(expectedResult)
+})
+
+test('sum() => 0', function(){
+    var expectedResult = 0
+    var actualResult = sum()
+    expect(actualResult).toBe(expectedResult)
+})
+
+test('sum([10,20,30],[40,50,60]) => 210', function(){
+    var expectedResult = 210
+    var actualResult = sum([10, 20, 30], [40, 50, 60])
+    expect(actualResult).toBe(expectedResult)
+})
+
+test('sum([10,"20",30],[40,50,"abc"]) => 150', function(){
+    var expectedResult = 150
+    var actualResult = sum([10, "20", 30], [40, 50, "abc"])
+    expect(actualResult).toBe(expectedResult)
+})
+
+test('sum([10,["20",30]],[40,50,60, "abc"]) => 210', function(){
+    var expectedResult = 210
+    var actualResult = sum([10, ["20", 30]], [40, 50, 60, "abc"])
+    expect(actualResult).toBe(expectedResult)
+})
+
+test('sum(function () { return 10; }, function () { return 20; }) => 30', function(){
+    var expectedResult = 30
+    var actualResult = sum(function () { return 10; }, function () { return 20; })
+    expect(actualResult).toBe(expectedResult)
+})
+
+test('sum(function () { return [10, ["20", 30]]; }, function () { return [40, 50, 60, "abc"]; }) => 210', function(){
+    var expectedResult = 210
+    var actualResult = sum(function () { return [10, ["20", 30]]; }, function () { return [40, 50, 60, "abc"]; })
+    expect(actualResult).toBe(expectedResult)
+})
+
+test('sum([function () { return [10, ["20", 30]]; }, function () { return [40, 50, 60, "abc"]; }]) => 210', function(){
+    var expectedResult = 210
+    var actualResult = sum([function () { return [10, ["20", 30]]; }, function () { return [40, 50, 60, "abc"]; }])
+    expect(actualResult).toBe(expectedResult)
+})
---
+++ 03-language-exercise/readme.md
@@ -0,0 +1,12 @@
+# Application setup
+
+```
+npm init -y
+npm install jest
+
+modify the package.json file as follows
+"scripts" : {
+    "test" : "jest"
+}
+
+```
