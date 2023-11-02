7112eaa456b51be32ee1125265c61c4b97d228c3
Magesh Kuppan <tkmagesh77@gmail.com>
2023-11-03 02:52:22 +0530

    Handling errors in async operations (using "error first callback" technique)

--- 07-async-programming/async-demos.js
+++ 07-async-programming/async-demos.js
@@ -16,21 +16,73 @@
     window['addSyncClient'] = addSyncClient;
 
     // async
-    function addAsync(x,y){
+    function addAsync(x,y, callbackFn){
         console.log(`   [@service] processing ${x} and ${y}`)
         setTimeout(function(){
             let result = x + y;
             console.log(`   [@service] returning result`)
-            return result;
+            // return result // cannot "return" from a function that will be executed in future
+            callbackFn(result);
         }, 5000);
     }
 
     function addAsyncClient(x,y){
         console.log(`[@client] triggering addAsync`)
-        let result = addAsync(x,y);
-        console.log(`[@client] result = ${result}`)
+        addAsync(x,y, function(result){
+            console.log(`[@client] result = ${result}`)
+        });
     }
 
     window['addAsyncClient'] = addAsyncClient;
+
+    // sync
+    function divideSync(x,y){
+        console.log(`   [@service] processing ${x} and ${y}`)
+        if (y === 0) 
+            throw new Error('cannot divide by zero');
+        let result = x / y;
+        console.log(`   [@service] returning result`)
+        return result;
+    }
+
+    function divideSyncClient(x,y){
+        console.log(`[@client] triggering divideSync`)
+        try{
+            let result = divideSync(x,y);
+            console.log(`[@client] result = ${result}`)
+        } catch(e){
+            console.log(`[@client] caught exception: ${e}`)
+        }
+    }
+    window['divideSyncClient'] = divideSyncClient;
+
+    // Handling errors in async operations (using "error first callback" technique)
+    // async
+    function divideAsync(x,y, callbackFn){
+        console.log(`   [@service] processing ${x} and ${y}`)
+        setTimeout(() => {
+            if (y === 0) {
+                // throw new Error('cannot divide by zero'); // cannot "throw" an exception from a function that will be executed in future
+                let err = new Error('cannot divide by zero');
+                callbackFn(err, null);
+                return;
+            }
+            let result = x / y;
+            console.log(`   [@service] returning result`)
+            callbackFn(null, result);
+        }, 5000);
+    }
+
+    function divideAsyncClient(x,y){
+        console.log(`[@client] triggering divideAsync`)
+        divideAsync(x,y, function(err, result){
+            if (err){
+                console.log(`[@client] caught exception: ${err}`)
+                return
+            }
+            console.log(`[@client] result = ${result}`)
+        });
+    }
+    window['divideAsyncClient'] = divideAsyncClient;
 
 })()
